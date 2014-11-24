f1871c2aa5a4b1bde865036e017311437f5e1c01
Jonathan Chang <jonoschang7@gmail.com>
2014-11-24 17:25:43 -0500

    create routes for games

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, :type => :controller do
+  describe 'Get #index' do
+  	it "populates an array with all the games" do
+  		game1 = create(:game)
+  		game2 = create(:game2)
+  		get :index
+  		expect(assigns(:games)).to match_array([game1, game2])
+  	end
 
+  	it "renders the :index view"
+  end
 end
--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -5,6 +5,11 @@ FactoryGirl.define do
     player2 "Peter"
     player3 "Pigpig"
     player4 "Julio"
+
+    factory :game2 do
+    	nickname "Game Second"
+    	player1 "Bobby"
+    end
   end
 
 end
