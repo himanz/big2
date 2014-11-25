b11fc6e1f92e2e8fb84992c2652b860678c13f44
Jonathan Chang <jonoschang7@gmail.com>
2014-11-24 23:17:16 -0500

    test if show action renders show template

--- spec/controllers/hands_controller_spec.rb
+++ spec/controllers/hands_controller_spec.rb
@@ -2,12 +2,18 @@ require 'rails_helper'
 
 RSpec.describe HandsController, :type => :controller do
   describe "Get #show" do
-
-  	it "assigns the requested hand to @hand" do
-  		@game = create(:game)
+    before do
+    	@game = create(:game)
   		@hand = create(:hand, game_id: @game)
   		get :show, id: @hand, game_id: @game.id
+    end
+
+  	it "assigns the requested hand to @hand" do
   		expect(assigns(:hand)).to eq @hand
+  	end
+
+  	it "renders the :show template" do
+  		expect(response).to render_template :show
   	end
   end
 end
