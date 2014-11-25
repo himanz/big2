c4e96135c9606c1c1bbe1a0ebc4f516eb80a6701
Jonathan Chang <jonoschang7@gmail.com>
2014-11-24 21:16:51 -0500

    add default values to players

--- db/migrate/20141124102438_create_games.rb
+++ db/migrate/20141124102438_create_games.rb
@@ -2,10 +2,10 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.string :nickname
-      t.string :player1
-      t.string :player2
-      t.string :player3
-      t.string :player4
+      t.string :player1, :default => "Player 1"
+      t.string :player2, :default => "Player 2"
+      t.string :player3, :default => "Player 3"
+      t.string :player4, :default => "Player 4"
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20141124102438) do
 
   create_table "games", force: true do |t|
     t.string   "nickname"
-    t.string   "player1"
-    t.string   "player2"
-    t.string   "player3"
-    t.string   "player4"
+    t.string   "player1",    default: "Player 1"
+    t.string   "player2",    default: "Player 2"
+    t.string   "player3",    default: "Player 3"
+    t.string   "player4",    default: "Player 4"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
