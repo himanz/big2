27849e8d89aeec7702f6a4b075c7086661d52215
Jonathan Chang <jonoschang7@gmail.com>
2014-11-25 19:43:52 -0500

    test score multiplier function

--- spec/models/hand_spec.rb
+++ spec/models/hand_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe Hand, :type => :model do
   end
 
   it "is invalid if score1 is under 0" do
-  	hand = Hand.new(score1: -1, score2: 13, score3: 0, score4: 1)
+  	hand = Hand.create(score1: -1, score2: 13, score3: 0, score4: 1)
     expect(hand.valid?).to be_falsey
     expect(hand.errors[:score1].size).to eq(1)  	
   end
 
   it "is invalid if score1 is over 65 " do
-  	hand = Hand.new(score1: 66, score2: 13, score3: 0, score4: 1)
+  	hand = Hand.create(score1: 66, score2: 13, score3: 0, score4: 1)
     expect(hand.valid?).to be_falsey
     expect(hand.errors[:score1].size).to eq(1)
   end
@@ -22,5 +22,41 @@ RSpec.describe Hand, :type => :model do
   	hand = Hand.new(score1: "hello", score2: 13, score3: 0, score4: 1)
     expect(hand.valid?).to be_falsey
     expect(hand.errors[:score1].size).to eq(1)
+  end
+
+  it "score multiplier should return 65 on 13" do
+    hand = create(:hand)
+    score = hand.score_multiplier(13)
+    expect(score).to eq(65)
+  end
+
+  it "score multiplier should return 48 on 12" do
+    hand = create(:hand)
+    score = hand.score_multiplier(12)
+    expect(score).to eq(48)
+  end
+
+  it "score multiplier should return 30 on 10" do
+    hand = create(:hand)
+    score = hand.score_multiplier(10)
+    expect(score).to eq(30)
+  end
+
+  it "score multiplier should return 12 on 6" do
+    hand = create(:hand)
+    score = hand.score_multiplier(6)
+    expect(score).to eq(12)
+  end
+
+  it "score multiplier should return 2 on 2" do
+    hand = create(:hand)
+    score = hand.score_multiplier(2)
+    expect(score).to eq(2)
+  end
+
+  it "score multiplier should return 0 on nil" do
+    hand = create(:hand)
+    score = hand.score_multiplier(nil)
+    expect(score).to eq(0)
   end
 end
